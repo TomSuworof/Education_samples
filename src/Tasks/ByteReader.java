4ffcc5a96cb894f5d3e0227177caba9f585dc91a
TomSuworof <rc1995king@gmail.com>
2020-07-01 21:42:42 +0300

    Test update

---
+++ src/Tasks/ByteReader.java
@@ -0,0 +1,38 @@
+package Tasks;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ByteReader {
+    public static void main(String[] args) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fileReader = new FileInputStream(bufferedReader.readLine())) {
+            HashMap<Integer, Integer> freq = new HashMap<>();
+            while (fileReader.available() > 0) {
+                int count = fileReader.read();
+                if (freq.containsKey(count)) {
+                    freq.put(count, freq.get(count) + 1);
+                } else {
+                    freq.put(count, 1);
+                }
+            }
+            Integer max_freq = Collections.max(freq.values());
+            Integer max_freq_val;
+            for (Map.Entry<Integer, Integer> pair : freq.entrySet()) {
+                Integer curr_val = pair.getKey();
+                Integer curr_freq = pair.getValue();
+                if (curr_freq.equals(max_freq)) {
+                    max_freq_val = curr_val;
+                    System.out.print(max_freq_val + " ");
+                }
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
--- src/Tasks/Solution.java
+++ src/Tasks/Solution.java
@@ -2,19 +2,21 @@ package Tasks;
 
 public class Solution {
     public static void main(String[] args) {
-        String s = "1100";
-        String s1 = "11000";
-        String s2 = "111000";
-        String s3 = "100000";
-        String s4 = "1111110";
-        String s5 = "10";
+        {
+            String s = "1100";
+            String s1 = "11000";
+            String s2 = "111000";
+            String s3 = "100000";
+            String s4 = "1111110";
+            String s5 = "10";
 
-        System.out.println(solve(s));
-        System.out.println(solve(s1));
-        System.out.println(solve(s2));
-        System.out.println(solve(s3));
-        System.out.println(solve(s4));
-        System.out.println(solve(s5));
+            System.out.println(solve(s));
+            System.out.println(solve(s1));
+            System.out.println(solve(s2));
+            System.out.println(solve(s3));
+            System.out.println(solve(s4));
+            System.out.println(solve(s5));
+        }
     }
     public static int solve(String s) {
         int zer =0;
