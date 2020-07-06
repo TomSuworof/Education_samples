80ad03e29492b38240eef9a3be11b8baca85eac3
TomSuworof <rc1995king@gmail.com>
2020-07-06 13:30:22 +0300

    Some new

--- src/Tasks/BinarySearch/CountingDinosaurs.java
+++ src/Tasks/BinarySearch/CountingDinosaurs.java
@@ -2,9 +2,19 @@ package Tasks.BinarySearch;
 
 public class CountingDinosaurs {
     public static void main(String[] args) {
+        String a1 = "cdcjchvjjevcgjcdedgcchvrie";
+        String a2 = "chbejhbcjnjjwdbxhejcehjcehcehx";
+        String a3 = "ZZqBwgkqAIXoEmxGGJlvgQlEMktPgumTWsFzfXidNhkuFkhNMZwFQfcSHWZWYFflwfBowLLtEIngguwUnCilzUqAcdpdyfxWDzjbPFYSFPpBvxiZxSBZNYYkusCcWHC";
 
+        String d1 = "c";
+        String d2 = "cc";
+        String d3 = "gmjYBiVKIBBziAxpwwMZmFlKuVTEOebxSHUIrRYRKtDmuwA";
+
+        System.out.println(solve(a1, d1));
+        System.out.println(solve(a2, d2));
+        System.out.println(solve(a3, d3));
     }
-    public int solve(String animals, String dinosaurs) {
+    static int solve(String animals, String dinosaurs) {
         String dino_unique = uni(dinosaurs);
         char[] dino_char = dino_unique.toCharArray();
         char[] ani_char = animals.toCharArray();
@@ -19,15 +29,14 @@ public class CountingDinosaurs {
         return count;
     }
 
-    public String uni(String input) {
-        String res = String.valueOf(input.charAt(0));
-        for (int i = 1; i < input.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (input.charAt(i) != input.charAt(j)) {
-                    res += input.charAt(i);
-                }
+    static String uni(String input) {
+        String res = "";
+        for (int i = 0; i < input.length(); i++) {
+            if (!res.contains(String.valueOf(input.charAt(i)))) {
+                res += String.valueOf(input.charAt(i));
             }
         }
+        System.out.println("Unique dinos: " + res);
         return res;
     }
 }
