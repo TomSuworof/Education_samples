99e56abc773f13d6a45360b942a54c4cc4b14393
TomSuworof <rc1995king@gmail.com>
2020-07-22 13:03:08 +0300

    New lines of code were added

--- src/Tasks/BinarySearch/SudokuValidator.java
+++ src/Tasks/BinarySearch/SudokuValidator.java
@@ -2,9 +2,6 @@ package Tasks.BinarySearch;
 
 /*
  * https://binarysearch.io/problems/Sudoku-Validator
- *
- * not solved
- *
  */
 
 import java.util.Arrays;
@@ -25,24 +22,63 @@ public class SudokuValidator {
     }
 
     static boolean solve(int[][] matrix) {
-        int[] row_true = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-//        // sum of 1 - 9 = 45
-//        int true_sum = 45;
+        System.out.println("------------------------");
+        int[] true_array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         // checking each row
         for (int i = 0; i < 9; i++) {
             int[] curr_row = matrix[i].clone();
+            System.out.println(Arrays.toString(curr_row));
+            System.out.println("--");
             Arrays.sort(curr_row);
-            if (!Arrays.equals(curr_row, row_true)) {
+            if (!Arrays.equals(curr_row, true_array)) {
                 return false;
             }
         }
 
+        System.out.println("------------------------");
+
         // checking each column
         for (int i = 0; i < 9; i++) {
-
+            int[] curr_column = new int[9];
+            for (int j = 0; j < 9; j++) {
+                curr_column[j] = matrix[j][i];
+            }
+            System.out.println(Arrays.toString(curr_column));
+            System.out.println("--");
+            Arrays.sort(curr_column);
+            if (!Arrays.equals(curr_column, true_array)) {
+                return false;
+            }
         }
 
-        return false;
+        System.out.println("------------------------");
+
+        // checking each box
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int[] curr_box = new int[9];
+                curr_box[0] = matrix[i * 3 + 0][j * 3 + 0];
+                curr_box[1] = matrix[i * 3 + 0][j * 3 + 1];
+                curr_box[2] = matrix[i * 3 + 0][j * 3 + 2];
+
+                curr_box[3] = matrix[i * 3 + 1][j * 3 + 0];
+                curr_box[4] = matrix[i * 3 + 1][j * 3 + 1];
+                curr_box[5] = matrix[i * 3 + 1][j * 3 + 2];
+
+                curr_box[6] = matrix[i * 3 + 2][j * 3 + 0];
+                curr_box[7] = matrix[i * 3 + 2][j * 3 + 1];
+                curr_box[8] = matrix[i * 3 + 2][j * 3 + 2];
+
+                System.out.println(Arrays.toString(curr_box));
+                System.out.println("--");
+                Arrays.sort(curr_box);
+                if (!Arrays.equals(curr_box, true_array)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
