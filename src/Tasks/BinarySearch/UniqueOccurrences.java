78b4177b18d5e5d20d4dba96dfa4bf32543464ad
TomSuworof <rc1995king@gmail.com>
2020-07-23 14:33:46 +0300

    New lines of code were added

---
+++ src/Tasks/BinarySearch/UniqueOccurrences.java
@@ -0,0 +1,56 @@
+package Tasks.BinarySearch;
+
+/*
+ * https://binarysearch.io/problems/Unique-Occurrences
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueOccurrences {
+    public static void main(String[] args) {
+        int[] nums0 = {5, 3, 1, 8, 3, 1, 1, 8, 8, 8};
+        int[] nums1 = {-3, -1, -1, -1, -2, -2};
+        int[] nums2 = {3, 1, 1, 2, 2, 2, 3};
+
+        System.out.println(solve(nums0)); // true
+        System.out.println(solve(nums1)); // true
+        System.out.println(solve(nums2)); // false;
+    }
+
+    static boolean solve(int[] nums) {
+        Arrays.sort(nums);
+        ArrayList<Integer> occurrences = new ArrayList<>();
+
+//        System.out.println(Arrays.toString(nums));
+
+        int current = nums[0];
+        int count = 0;
+        for (int num : nums) {
+//            System.out.print("Current: " + current);
+//            System.out.print(" Num: " + num);
+//            System.out.println(" Count: " + count);
+            if (num == current) {
+                count++;
+            } else {
+                occurrences.add(count);
+                current = num;
+                count = 1;
+            }
+        }
+        occurrences.add(count);
+
+//        System.out.println(occurrences);
+
+//        for (int i = 0; i < occurrences.size(); i++) {
+//            for (int j = i + 1; j < occurrences.size(); j++) {
+//                if (i != j && occurrences.get(i).intValue() == occurrences.get(j).intValue()) {
+//                    return false;
+//                }
+//            }
+//        }
+
+        return new HashSet(occurrences).size() == occurrences.size();
+    }
+}
